498cf1f7fea11fdd949cc23091f2cdf371619161
yoo94 <ahy0119@naver.com>
2021-03-27 01:26:04 +0900

    nodemon

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const server = express(); //서버만들기
-//GET  서버로 보낼 때 위에 표시
-//POST  ID PWD같은거
-//DELETE 탈퇴같은거
-//PUT   update개념
+const hbs = require("express-handlebars");
 
-server.use(express.static(__dirname+"/public"));
-//서버가 돌아갈떄 여기를 먼저 들러서 순차적으로 내려감, 그래서 css같은 파일 먹이려면 위에서 public 들렷다가라고 해야함
+const server = express();
 
-server.get("/",(req,res)=>{
-    console.log(req.user);
-  res.sendFile(__dirname+"/index.html");
+server.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layouts.hbs",
+    partialsDir: "partials",
+  })
+);
+server.set("view engine", "hbs");
+server.use(express.static(__dirname + "/public"));
+
+server.get("/", (req, res) => {
+  res.render("home", {
+    message: "Hello from node.js",
+  });
 });
 
-server.get("/about",(req,res)=>{
-res.sendFile(__dirname+"/about.html");
+server.listen(3000, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
 });
-
-server.use((req,res)=>{
-    res.sendFile(__dirname + "/404.html");
-});
-
-server.listen(3000,(err)=>{
-    if(err) return console.log(err);
-    console.log("the server is listening on port 3000");
-})
