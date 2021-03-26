e3b704ea6728b10b86e50f5023c05c0d0b176c18
yoo94 <ahy0119@naver.com>
2021-03-27 00:38:43 +0900

    name change

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const server = express(); //서버만들기
 //POST  ID PWD같은거
 //DELETE 탈퇴같은거
 //PUT   update개념
-server.get("/",(req,res)=>{
-  res.sendFile   
-})
 
+server.use(express.static(__dirname+"/public"));
+//서버가 돌아갈떄 여기를 먼저 들러서 순차적으로 내려감, 그래서 css같은 파일 먹이려면 위에서 public 들렷다가라고 해야함
+
+server.get("/",(req,res)=>{
+    console.log(req.user);
+  res.sendFile(__dirname+"/index.html");
+});
+
+server.get("/about",(req,res)=>{
+res.sendFile(__dirname+"/about.html");
+});
+
+server.use((req,res)=>{
+    res.sendFile(__dirname + "/404.html");
+});
 
 server.listen(3000,(err)=>{
     if(err) return console.log(err);
